5c1b3d75904af2a11a5ca51f5dca41438ad933ed
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-06-11 13:00:42 +0200

    operatory

---
+++ wprowadzanie_danych.py
@@ -0,0 +1,10 @@
+import math
+# wyświetlenie pytania i pobranie danych do zmiennej a
+a = input("Podaj swoje imię: ")
+print(f'{a} - jest fajnym gościem!!!')  # wyświetlenie odpowiedzi
+
+promien = float(input('podaj promień:'))
+# print('pole kola wynosi: ', 3.14*promien**2)
+
+
+print(f'pole kola wynosi: {math.pi*promien**2}')
--- zmienne_str.py
+++ zmienne_str.py
@@ -15,3 +15,24 @@ nazwisko = "Kowalski"
 # literka f przed znakiem apostrofu lub cudzysłowia informuje, ze w ciągu podane w klamrowym nawiasie nazwy zmiennych mają pobierać jej wartość
 imie_nazwisko = f'{imie} 23, {nazwisko} to fajny gość'
 print(imie_nazwisko)
+
+pierwsza_litera = imie_nazwisko[0]  # pobranie 1 litery z stringa
+print(pierwsza_litera)
+
+zakres_liter = imie_nazwisko[0:3]  # pobranie pierwszych 3 liter
+print(zakres_liter)
+
+zakres_liter = imie_nazwisko[4:]  # bez Jana do końca
+print(zakres_liter)
+
+zakres_ostatnich_4_liter = imie_nazwisko[-4:]  # pobieramy 4 ostatnie
+print(zakres_ostatnich_4_liter)
+co_druga = imie_nazwisko[::2]
+print(co_druga)
+
+zwierze = 'biały słoń z kłami'
+print(zwierze.capitalize())
+print(zwierze.title())
+substring = 'a'
+zlicz = zwierze.count(substring)
+print(zlicz)
