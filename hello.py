59efbbbbca177a61f6f305fb852e8b6ecab56fa4
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-06-11 11:19:10 +0200

    commit 3

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("cześć")
+print("cześć")  # wyrzucenie wartości w nawiasie na ekran
+print(3+4)
+print(5-8)
+print(10*5)
+print(10/2)
+print(10//3)
+# print(10/0) -dzielenie powoduje bład
+print(3*11, 5)  # wydruk na ekran dwóch wartości
+print(3*11.5)
---
+++ zmienne.py
@@ -0,0 +1,30 @@
+a = 20  # deklarujemy zmienną a
+b = 10  # deklarujemy zmienną b
+
+suma = a+b  # deklarujemy zmienną suma oraz przypisujemy jej wartość a+b
+print(suma)  # wyświetlamy wartość suma
+print(suma, type(suma))  # wyświetlamy wartość oraz wyświetlamy typ danych
+
+a = 1.23
+b = 33.44
+suma = a/b
+print(suma, type(suma))
+
+
+a = 20
+print(a, type(a))  # typ int
+
+a = 10.5
+print(a, type(a))  # typ float
+
+a = -10
+print(a, type(a))  # typ int
+
+a = False
+print(a, type(a))  # typ bool
+
+a = "dwadzieścia"
+print(a, type(a))  # typ str
+
+# suma = a/b  # - ten fragment powoduje błąd ze wględu na brak operacji dzielenia str/float
+print(suma)
