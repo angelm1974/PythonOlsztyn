e38e097c8e682b5195d103b02f47db3806c583fd
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-06-25 08:26:18 +0200

    zmiana

---
+++ mojaklasa.py
@@ -0,0 +1,16 @@
+class kot:
+    wiek=3
+    
+Alek = kot #utworzenie tworzenie instancji
+print('Kot: ',dir(kot),'\n') 
+
+print('Alek: ',dir(Alek),'\n')
+
+
+class pies(object):
+    wiek=0
+
+Burek=pies
+print('Pies: ',dir(pies),'\n')
+
+print('Burek: ',dir(Burek),'\n')
